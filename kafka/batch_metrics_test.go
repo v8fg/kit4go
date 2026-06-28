1080c7c62560b6d706aa4463dcfcd1826fa66b57
xwi88 <278810732@qq.com>
2026-06-29 01:36:50 +0800

    fix: goimports ordering + !franzgo tag in batch_metrics_test.go

--- kafka/batch_metrics_test.go
+++ kafka/batch_metrics_test.go
@@ -1,10 +1,13 @@
+//go:build !franzgo
+
 package kafka
 
 import (
 	"context"
 	"testing"
-	"github.com/IBM/sarama/mocks"
+
 	"github.com/IBM/sarama"
+	"github.com/IBM/sarama/mocks"
 )
 
 func TestSendBatch_MetricsTrackRealRecords(t *testing.T) {
